133ef5643859c5c1cd0208039b7b6458e801c3b6
wf <wf@mlf.com>
2020-05-25 23:19:08 +0800

    Signed-off-by: wf <wf@mlf.com>

--- carcenter/views.py
+++ carcenter/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from carcenter.car import Car
+
+gCar = Car()
  
 def index(request):
     return render(request, "index.html", {})
